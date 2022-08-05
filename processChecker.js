3ea47e663359cb9141087c7f0d3badf2259d6c28
Simon Studen <56488606+sstuden2255@users.noreply.github.com>
2022-08-05 15:37:18 -0700

    Create processChecker.js

---
+++ processChecker.js
@@ -0,0 +1,23 @@
+const exec = require('child_process').exec
+
+function isRunning(win, mac, linux){
+    return new Promise(function(resolve, reject){
+        const plat = process.platform
+        const cmd = plat == 'win32' ? 'tasklist' : (plat == 'darwin' ? 'ps -ax | grep ' + mac : (plat == 'linux' ? 'ps -A' : ''))
+        const proc = plat == 'win32' ? win : (plat == 'darwin' ? mac : (plat == 'linux' ? linux : ''))
+        if(cmd === '' || proc === ''){
+            resolve(false)
+        }
+        exec(cmd, function(err, stdout, stderr) {
+            if (plat == 'win32') {
+                resolve(stdout.toLowerCase().indexOf(proc.toLowerCase()) > -1)
+            } else if  (plat == 'darwin') {
+                resolve(stdout.length > 0)
+            }
+        })
+    })
+}
+
+//isRunning('zoom.exe', 'zoom.us', 'NA').then((v) => console.log(v))
+
+module.exports = { isRunning }
